de0aa4e2802a1e1026862644f6c4948a2b5def11
blyndusk <alexandredelaloydev@gmail.com>
2019-11-13 15:47:52 +0100

    add submit & form globam style

--- src/pages/edit/styledEditPageComponents.js
+++ src/pages/edit/styledEditPageComponents.js
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
-export const ScForm = styled.form`
-    display: flex;
-    flex-direction: column;
-`;
 export const ScH2 = styled.h2`
     margin: 30px;
     font-family: Open Sans;
@@ -14,6 +10,11 @@ export const ScH2 = styled.h2`
     align-items: center;
 
     color: #000000;
+`;
+
+export const ScForm = styled.form`
+    display: flex;
+    flex-direction: column;
 `;
 
 export const ScInput = styled.input`
@@ -41,7 +42,6 @@ export const ScNav = styled.nav`
 `;
 
 export const ScButton = styled.button`
-
     background: ${props => props.isSelected ? "#03CEA4" : "#fff"};
     color: ${props => props.isSelected ? "#fff" : "#A5A5A5"};
     margin: 0 15px 30px;
@@ -60,6 +60,51 @@ export const ScButton = styled.button`
     :hover {
         color: #fff;
         background: #03CEA4;
-    }   
-}
+    }
+`;
+
+export const ScGradeInput = styled.div`
+    background: #FFFFFF;
+    min-height: 50px;
+    margin: 0 30px 30px;
+    padding: 0 40px;
+    font-family: Open Sans;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 25px;
+    box-shadow: 0px 2px 5px -1px rgba(122,122,122,1);
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h3 {
+        font-size: 18px;
+    }
+`;
+export const ScGradebutton = styled.button`
+    width: 32px;
+    height: 32px;
+    margin: 0 5px;
+    background: #C4C4C4;
+    border-radius: 100%;
+    border: none;
+    color: #fff;
+    font-family: Open Sans;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 33px;
+`;
+
+export const ScSubmitButton = styled.input`
+    width: 340px;
+    background: #03CEA4;
+    border-radius: 43.5px;
+    border: none;
+    padding: 14px;
+    text-transform: uppercase;
+    font-family: Open Sans;
+    font-weight: bold;
+    font-size: 15px;
+    line-height: 20px;
+    color: #FFFFFF;
 `;
