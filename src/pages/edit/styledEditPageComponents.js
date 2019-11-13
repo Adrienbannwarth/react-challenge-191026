364696ca4346e70ba3b2641c77c3ead7ab8da6c9
blyndusk <alexandredelaloydev@gmail.com>
2019-11-13 15:04:29 +0100

    add form, button h2 & input styled components

---
+++ src/pages/edit/styledEditPageComponents.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const ScForm = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+export const ScH2 = styled.h2`
+    margin: 30px;
+    font-family: Open Sans;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 33px;
+    display: flex;
+    align-items: center;
+
+    color: #000000;
+`;
+
+export const ScInput = styled.input`
+    background: #FFFFFF;
+    width: calc(100% - 2 * 30px);
+    min-height: 50px;
+    margin: 0 30px 30px;
+    box-shadow: 0px 2px 5px -1px rgba(122,122,122,1);
+    border-radius: 10px;
+    border: none;
+    ::placeholder {
+        padding: 40px;
+        font-family: Open Sans;
+        font-size: 18px;
+        line-height: 25px;
+        display: flex;
+        align-items: center;
+
+        color: #A5A5A5;
+    }
+`;
+
+export const ScNav = styled.nav`
+    display: flex;
+`;
+
+export const ScButton = styled.button`
+
+    background: ${props => props.isSelected ? "#03CEA4" : "#fff"};
+    color: ${props => props.isSelected ? "#fff" : "#A5A5A5"};
+    margin: 0 15px 30px;
+    padding: 10px 25px;
+    box-shadow: 0px 2px 5px -1px rgba(122,122,122,1);
+    border-radius: 10px;
+    border: none;
+    font-family: Open Sans;
+    font-size: 18px;
+    line-height: 25px;
+    display: flex;
+    justifiy-content: center;
+    align-items: center;
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+    :hover {
+        color: #fff;
+        background: #03CEA4;
+    }   
+}
+`;
